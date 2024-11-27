80d87b49a3b2d243efad2127e56631108cf92d0c
GabrielCavalcante <103294728+gabri3l-cavlcant3@users.noreply.github.com>
2024-11-27 16:12:14 -0300

    config folder

---
+++ configurations/ossn.config.db.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (openteknik.com).ossn
+ * @author    OSSN Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+
+// replace localhost with your database host name;
+$Ossn->host = 'localhost';
+
+// replace 3306 with your database host name;
+$Ossn->port = '3306';
+
+// replace ossndb with your database username;
+$Ossn->user = 'ossndb';
+
+// replace ZjE2YTg0MG with your database password;
+$Ossn->password = 'ZjE2YTg0MG';
+
+// replace ossndb with your database name;
+$Ossn->database = 'ossndb';
